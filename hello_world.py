33d2754078b3f588e6f62b87e7618fc40890b60f
nirvana47 <nirvana47@gmail.com>
2023-01-28 22:29:38 +0000

    trying new ways to do math

--- hello_world.py
+++ hello_world.py
@@ -5,8 +5,10 @@ Print "Hello World" in Python
 print("Hello, World!")
 print("Is anybody out there?")
 
-num1 = 10
-num2 = 15
-num3 = num1 + num2
+# num1 = 10
+# num2 = 15
+# num3 = num1 + num2
 
-print(num3)
+# print(num3)
+
+print (10 + 25 / 56 * 0.334)
