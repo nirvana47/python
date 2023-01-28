40e4d16f374cc62f0a954dde814056576c0a39cb
nirvana47 <nirvana47@gmail.com>
2023-01-28 22:53:06 +0000

    taking it back to hello world

--- hello_world.py
+++ hello_world.py
@@ -3,12 +3,4 @@ Print "Hello World" in Python
 
 """
 print("Hello, World!")
-print("Is anybody out there?")
 
-# num1 = 10
-# num2 = 15
-# num3 = num1 + num2
-
-# print(num3)
-
-print (10 + 25 / 56 * 0.334)
