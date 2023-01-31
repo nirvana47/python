f0c5726c16bb8966b716ea2585ed92c1dc804eab
nirvana47 <nirvana47@gmail.com>
2023-01-31 19:21:50 +0000

    getting iPad model names

--- webscraping/ipad_scraping.py
+++ webscraping/ipad_scraping.py
@@ -3,7 +3,11 @@ import requests
 
 html_text = requests.get('https://www.apple.com/shop/refurbished/ipad/ipad').text
 # print(html_text)
+
 soup = BeautifulSoup(html_text, 'lxml')
 ipads = soup.find_all('h3')
-print(ipads)
-# print(soup.prettify())
+# print(ipads)
+
+for ipad in ipads:
+    ipad = ipad.find('a').text.replace('\n', '').replace('Refurbished ', '')
+    print(ipad)
