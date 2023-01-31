e23be1acb4ddab1b4b9a63d037d059dbe6368151
nirvana47 <nirvana47@gmail.com>
2023-01-31 05:25:09 +0000

    static HTML scraping done

--- webscraping/main.py
+++ webscraping/main.py
@@ -3,5 +3,14 @@ from bs4 import BeautifulSoup
 with open('/workspaces/python/webscraping/home.html', 'r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find_all('h5')
-    print(tags)
+    # course_html_tags = soup.find_all('h5')
+    # for course in course_html_tags:
+    #     print(course.text)
+    course_cards = soup.find_all('div', class_='card')
+    # print(course_cards)
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        # print(course_name)
+        # print(course_price)
+        print(f'{course_name} costs {course_price}')
