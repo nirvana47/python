bcb7d6fa4b0669cd565d33deabc5421831e4f7e0
nirvana47 <nirvana47@gmail.com>
2023-01-31 18:51:56 +0000

    figured out how to get list of iPads

--- webscraping/ipad_scraping.py
+++ webscraping/ipad_scraping.py
@@ -2,4 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.apple.com/shop/refurbished/ipad/ipad').text
-print(html_text)
+# print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+ipads = soup.find_all('h3')
+print(ipads)
+# print(soup.prettify())
